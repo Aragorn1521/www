89862cc6c460ad5689a3a247aa5a74f27ed02d58
Aragorn52 <pobeda.deda1945@gmail.com>
2021-04-22 11:56:15 +0400

    one4

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -8,9 +8,7 @@ Class NewsController
         $newsList = [];
         $newsList = News::getNewsList();
         
-        echo '<pre>';
-        print_r($newsList);
-        echo '<pre>';
+        require_once ROOT.'/views/news/index.php';
         return true;
     }
     
